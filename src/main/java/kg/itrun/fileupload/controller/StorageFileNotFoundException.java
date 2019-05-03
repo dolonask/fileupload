4356c044ae16397b505e46ed66a0d4267bb6fdc1
Dolon Ask <dolon91aa@gmail.com>
2019-05-03 17:54:24 +0600

    Initial commit

---
+++ src/main/java/kg/itrun/fileupload/controller/StorageFileNotFoundException.java
@@ -0,0 +1,14 @@
+package kg.itrun.fileupload.controller;
+
+import kg.itrun.fileupload.storage.StorageException;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
